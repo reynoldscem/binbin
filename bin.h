a1fc2ef09a321327da355446e0e1ec1032497dfa
Charlie Reynolds <reynoldscem@gmail.com>
2015-08-02 14:13:13 +0100

    Initial commit

---
+++ bin.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define EMBRACE int main(void) {
+#define PASSIVE while
+#define AGGRESSION (true)
+#define FOR puts
+#define LIFE ("bin");
+#define PEOPLE }
